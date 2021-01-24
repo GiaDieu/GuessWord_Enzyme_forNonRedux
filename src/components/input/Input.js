09cc6ca8a6a3567d0f7e92aa07575e75b6ff563d
GiaDieu <giadieuly@gmail.com>
2021-01-24 12:57:14 +0700

    update - stateUpdateWithValueOfInputTesting

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -2,7 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Input = ({ secretWord }) => {
-  return <div data-test="component-input"></div>;
+  const [currentGuess, setCurrentGuess] = React.useState('');
+  return (
+    <div data-test="component-input">
+      <form className="form-inline">
+        <input
+          type="text"
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          placeholder="enter guess"
+          value={currentGuess}
+          onChange={(e) => setCurrentGuess(e.target.value)}
+        />
+        <button
+          data-test="submit-button"
+          type="submit"
+          className="btn btn-primary mb-2"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 Input.propTypes = {
