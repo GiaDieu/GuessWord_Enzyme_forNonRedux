92c417b8d425808aa92194960e33142752de2eb5
GiaDieu <giadieuly@gmail.com>
2021-01-24 16:52:49 +0700

    update - App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { findByTestAttr } from '../test/testUtils';
 import App from './App';
+import hookActions from './actions/hookActions';
+
+const mockGetSecretWord = jest.fn();
 
 /**
  * Setup Function for App Component
- * @returns {shallowWrapper}
+ * @returns {ReactWrapper}
  */
 
 const setup = () => {
-  return shallow(<App />);
+  mockGetSecretWord.mockClear();
+  hookActions.getSecretWord = mockGetSecretWord;
+
+  //use mount , because useEffect does not call on `shallow`
+  //https://github.com/airbnb/enzyme/issues/2086
+  return mount(<App />);
 };
 
-it('App renders without error', () => {
+test('App renders without error', () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, 'component-app');
   expect(component.length).toBe(1);
+});
+
+describe('getSecretWord calls', () => {
+  test('getSecretWord gets called on App mount', () => {
+    setup();
+    //check to see whether secret word was updated
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
 });
